9ec2b5dbfbe164ca952e810f51eae6bd7d555677
Mateusz <skipper126@interia.pl>
2017-03-11 00:23:30 +0100

    deklaracja metod

---
+++ env/herding.py
@@ -0,0 +1,22 @@
+import gym
+import numpy as np
+
+class Herding(gym.Env):
+
+    def __init__(self):
+        pass
+
+    def _step(self, action):
+        pass
+
+    def _reset(self):
+        pass
+
+    def _render(self, mode='human', close=False):
+        pass
+
+    def _close(self):
+        pass
+
+    def _seed(self, seed=None):
+        pass
